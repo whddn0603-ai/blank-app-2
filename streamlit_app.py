1aadf8147f1b36b6882957c470148ca936f874ca
whddn0603-ai <whddn0603@gmail.com>
2025-11-07 05:52:09 +0000

    코드종호햄03

--- streamlit_app.py
+++ streamlit_app.py
@@ -93,7 +93,7 @@ def main():
     st.set_page_config(page_title="기술보증기금 AI 비서", layout="wide", page_icon="🤖")
     st.image('data/123.jpg', width=600)
     st.markdown('---')
-    st.title("안녕하세요! RAG + Web을 활용한 '기술보증기금 AI 비서' 입니다")  
+    st.title("안녕하세요! 힘세고 강한 아침! 내 이름은 종호입니다")  
 
     with st.sidebar:
         openai_api = st.text_input("OPENAI API 키", type="password")
