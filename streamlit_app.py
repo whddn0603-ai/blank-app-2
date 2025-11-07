a902c2b9ba0b30c96f3a123288ef43bbf965d3e1
whddn0603-ai <whddn0603@gmail.com>
2025-11-07 05:38:39 +0000

    코드수정02-1

--- streamlit_app.py
+++ streamlit_app.py
@@ -129,9 +129,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-import streamlit as st
-
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
